681e414d523fa58c0dc78c28d801627f214e3ca8
James-Kelley-Galvanize <james.kelley@galvanize.com>
2023-01-06 14:55:40 -0700

    add first fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+const url = process.env.REACT_APP_API_URL || `localhost:8080`;
 
 function App() {
+  const [apiDependantState, setApiDependantState] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setApiDependantState(data);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +25,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {apiDependantState}
         </a>
       </header>
     </div>
